967b59521eb55359bb205c0796ec187bd52a812a
Arunabh Das <arunabhdas@gmail.com>
2021-12-31 02:18:40 -0500

    Created crud endpoint scaffold

--- server.ts
+++ server.ts
@@ -1,8 +1,26 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
+// get all products
 app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Awesome Possum'});
+});
+
+// create a product
+app.post('/', (req: Request, res: Response) => {
+    res.json({ message: 'Awesome Possum'});
+});
+
+// get a single product
+app.get('/:product_id', (req: Request, res: Response) => {
+    res.json({ message: 'Awesome Possum'});
+});
+
+// delete a single product
+app.delete('/:product_id', (req: Request, res: Response) => {
     res.json({ message: 'Awesome Possum'});
 });
 
